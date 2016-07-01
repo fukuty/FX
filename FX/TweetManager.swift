8e53732a8b7bfbbb6af84e4cb8ac109b6624f6bf
福田 翔 <fukudashou@fukuda-no-MacBook-Pro-2.local>
2016-07-01 17:30:18 +0900

    文字変更

---
+++ FX/TweetManager.swift
@@ -0,0 +1,13 @@
+//
+//  TweetManager.swift
+//  FX
+//
+//  Created by 福田 翔 on 2016/07/01.
+//  Copyright © 2016年 sho. All rights reserved.
+//
+
+import UIKit
+
+class TweetManager: NSObject {
+
+}
---
+++ FX/TweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  FX
+//
+//  Created by 福田 翔 on 2016/07/01.
+//  Copyright © 2016年 sho. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
