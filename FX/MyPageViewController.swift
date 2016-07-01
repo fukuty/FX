dc3ba110c08be08940eaaa01bc4eb914da3cb927
福田 翔 <fukudashou@fukuda-no-MacBook-Pro-2.local>
2016-07-01 18:55:46 +0900

    変更２

---
+++ FX/MyPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MyPageViewController.swift
+//  FX
+//
+//  Created by 福田 翔 on 2016/07/01.
+//  Copyright © 2016年 sho. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
