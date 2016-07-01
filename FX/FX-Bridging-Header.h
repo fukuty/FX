4339f948d69389349d8c51865a98c7dbce04efe7
福田 翔 <fukudashou@fukuda-no-MacBook-Pro-2.local>
2016-07-01 17:08:20 +0900

    初コミット

---
+++ FX/FX-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  FX-Bridging-Header.h
+//  FX
+//
+//  Created by 福田 翔 on 2016/07/01.
+//  Copyright © 2016年 sho. All rights reserved.
+//
+
+#ifndef FX_Bridging_Header_h
+#define FX_Bridging_Header_h
+#import <NCMB/NCMB.h>
+
+#endif /* FX_Bridging_Header_h */
---
+++ FX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FX
+//
+//  Created by 福田 翔 on 2016/07/01.
+//  Copyright © 2016年 sho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
